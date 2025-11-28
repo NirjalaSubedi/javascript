cc240c267e35d3ae8cccc30e4d2be75703844c96
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-28 07:16:50 +0545

    Add form fields for phone number, zip code, and password with validation

--- formValidation/script.js
+++ formValidation/script.js
@@ -3,9 +3,13 @@ const emailinput=document.getElementById('email');
 const form=document.getElementById('validate');
 const result=document.querySelectorAll('.result');
 
+
 //regular expression
 const usernameRegex=/^[a-zA-Z0-9]{5,20}$/;
 const emailRegex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
+const numberRegex=/^\d{10}$/;
+const zipCodeRegex=/^\d{5}(-\d{4})?$/;
+const passwordRegex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
@@ -33,5 +37,42 @@ form.addEventListener('submit',function(e){
     emailinput.style.borderColor="red";
     emailinput.style.color="red";
     }
+
+    if( numberRegex.test(phonenumber.value)){
+    result[2].textContent="Valid phoneNumber";
+    result[2].style.color="green";
+    phonenumber.style.borderColor="green";
+    phonenumber.style.color="green";
+   }else{
+    result[2].textContent="Invalid phoneNumber";
+    result[2].style.color="red";
+    phonenumber.style.borderColor="red";
+    phonenumber.style.color="red";
+    }
+    if( zipCodeRegex.test(ZipCode.value)){
+    result[3].textContent="Valid ZipCode";
+    result[3].style.color="green";
+    ZipCode.style.borderColor="green";
+    ZipCode.style.color="green";
+   }else{
+    result[3].textContent="Invalid ZipCode";
+    result[3].style.color="red";
+    ZipCode.style.borderColor="red";
+    ZipCode.style.color="red";
+    }
+    if( passwordRegex.test(password.value)){
+    result[4].textContent="Valid password";
+    result[4].style.color="green";
+    password.style.borderColor="green";
+    password.style.color="green";
+   }else{
+    result[4].textContent="Invalid password";
+    result[4].style.color="red";
+    password.style.borderColor="red";
+    password.style.color="red";
+    }
+
+
+
    console.log("submitted")
 })
---
+++ labwork/labwork15/labwork15.js
@@ -0,0 +1,14 @@
+const form=document.getElementById('registerform');
+const emailInput=document.getElementById('email');
+const ageInput=document.getElementById('age');
+const passwordInput=document.getElementById('password');
+const confirmPasswordInput=document.getElementById('confirmPassword');
+const zipcodeInput=document.getElementById('zipcode');
+const termsCheckbox=document.getElementById('terms');
+const urlInput=document.getElementById('url');
+const DateInput=document.getElementById('date');
+const fileuploadInput=document.getElementById('fileupload');
+const countrySelect=document.getElementById('country');
+const register=document.getElementByQuerySelectorAll('.registerresult');
+
+//regular Expressiuons
